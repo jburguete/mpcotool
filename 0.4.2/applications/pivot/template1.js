dbbde87e700f48ec228a3872e7cf88f92a54b76d
Javier Burguete <jburguete@eead.csic.es>
2014-11-26 12:26:28 +0100

    Adding pivot application files and improving article

---
+++ 0.4.2/applications/pivot/template1.js
@@ -0,0 +1,41 @@
+{
+  "towers" :
+  [
+    {
+      "length"      : 50.11,
+      "velocity"    : 0.02738,
+      "@variable1@" : @value1@,
+      "@variable2@" : @value2@,
+      "@variable3@" : @value3@,
+      "@variable4@" : @value4@
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.02824,
+      "@variable1@" : @value1@,
+      "@variable2@" : @value2@,
+      "@variable3@" : @value3@,
+      "@variable4@" : @value4@
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.03008,
+      "@variable1@" : @value1@,
+      "@variable2@" : @value2@,
+      "@variable3@" : @value3@,
+      "@variable4@" : @value4@
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.03753,
+      "@variable1@" : @value1@,
+      "@variable2@" : @value2@,
+      "@variable3@" : @value3@,
+      "@variable4@" : @value4@
+    }
+  ],
+  "cycle-time"     : 71.0,
+  "plot-time"     : 1.0,
+  "comp-time-step": 0.1,
+  "active-percent" : 27.48
+}
