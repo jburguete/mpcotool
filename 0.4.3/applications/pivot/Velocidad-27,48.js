edf5404156c657084c93855c56df72a7bbc14c1a
Javier Burguete <jburguete@eead.csic.es>
2014-12-08 08:23:05 +0100

    Adding some pivot applications cases and results

---
+++ 0.4.3/applications/pivot/Velocidad-27,48.js
@@ -0,0 +1,41 @@
+{
+  "towers" :
+  [
+    {
+      "length"      : 50.11,
+      "velocity"    : 0.02738,
+      "alpha1"      : 180.166,
+      "alpha2"      : 179.751,
+      "random"      : 0.136,
+      "boot-time"   : 3.32
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.02824,
+      "alpha1"      : 180.166,
+      "alpha2"      : 179.751,
+      "random"      : 0.136,
+      "boot-time"   : 3.32
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.03008,
+      "alpha1"      : 180.166,
+      "alpha2"      : 179.751,
+      "random"      : 0.136,
+      "boot-time"   : 3.32
+    },
+    {
+      "length"    : 50.11,
+      "velocity"  : 0.03753,
+      "alpha1"      : 180.166,
+      "alpha2"      : 179.751,
+      "random"      : 0.136,
+      "boot-time"   : 3.32
+    }
+  ],
+  "cycle-time"     : 71.0,
+  "plot-time"     : 1.0,
+  "comp-time-step": 0.1,
+  "active-percent" : 27.48
+}
