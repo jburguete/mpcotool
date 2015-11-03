0a81e75da8ecf0181f1528a03f635059081d8278
jburguete <jburguete@eead.csic.es>
2015-11-03 12:24:50 +0100

    Improving configuration and readme files

--- tests/test5/simulator.c
+++ tests/test5/simulator.c
@@ -1,13 +1,23 @@
+/*
+This is a test case to check the performance of optimization algorithms in an
+ill-conditioned problem
+
+The objective function is:
+f = 1 + (x - pi)^2 / 100 + (y - pi)^2 / 100 + sin(pi * (y - 0.5))
+
+The problem has 10 local minimums, located in very flat regions, on the interval
+x \in [-10, 10] and y \in [-10, 10]
+
+Absolute minimum is: x = pi, y = pi
+*/
+
 #include <stdio.h>
 #include <math.h>
 
-inline double sqr (double x)
-{
-	return x * x;
-}
-
 inline double objective (double x, double y)
 {
+	x -= M_PI;
+	y -= M_PI;
 	return 1. + 0.001 * (x * x + y * y) + sin(M_PI * y - M_PI_2);
 }
 
