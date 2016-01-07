0b54492cee85477d9f1ad5876be52f57db2329de
Javier Burguete <jburguete@eead.csic.es>
2016-01-07 07:50:22 +0100

    Replacing some tests

--- tests/process.c
+++ tests/process.c
@@ -5,15 +5,22 @@
 int main (int argn, char **argc)
 {
   FILE *filein, *fileout;
-  double x, y, z, m, dx, dy;
+  double x, y, x0, y0, z, m, dx, dy;
   unsigned int i;
   filein = fopen (argc[1], "r");
   fileout = fopen (argc[2], "w");
+  if (argn == 3)
+	x0 = y0 = M_PI_4;
+  else
+	{
+	  x0 = atof (arg[3]) + M_PI_4;
+	  y0 = atof (arg[4]) + M_PI_4;
+	}
   m = DBL_MAX;
   for (i = 1; fscanf (filein, "%lf%lf%lf", &x, &y, &z) == 3; ++i)
     {
-	  dx = x - M_PI_4;
-	  dy = y - M_PI_4;
+	  dx = x - x0;
+	  dy = y - y0;
 	  z = sqrt (dx * dx + dy * dy);
       m = fmin (m, z);
       fprintf (fileout, "%u %le %le %le %le\n", i, x, y, z, m);
