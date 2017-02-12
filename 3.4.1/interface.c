cb622e5b320df157e59a9b13c0feaa081c828634
Javier Burguete <jburguete@eead.csic.es>
2017-02-12 22:35:21 +0100

    Improving compilation and external library interface

--- 3.4.1/interface.c
+++ 3.4.1/interface.c
@@ -1078,7 +1078,7 @@ window_about ()
      "Javier Burguete Tolosa <jburguete@eead.csic.es> "
      "(english, french and spanish)\n"
      "Uğur Çayoğlu (german)",
-     "version", "3.4.0",
+     "version", "3.4.1",
      "copyright", "Copyright 2012-2017 Javier Burguete Tolosa",
      "logo", window->logo,
      "website", "https://github.com/jburguete/mpcotool",
---
+++ 3.4.1/main.c
@@ -0,0 +1,78 @@
+/*
+MPCOTool:
+The Multi-Purposes Calibration and Optimization Tool. A software to perform
+calibrations or optimizations of empirical parameters.
+
+AUTHORS: Javier Burguete and Borja Latorre.
+
+Copyright 2012-2017, AUTHORS.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY AUTHORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+/**
+ * \file main.c
+ * \brief Main source file.
+ * \authors Javier Burguete and Borja Latorre.
+ * \copyright Copyright 2012-2017, all rights reserved.
+ */
+#define _GNU_SOURCE
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+#include <gsl/gsl_rng.h>
+#include <libxml/parser.h>
+#include <libintl.h>
+#include <glib.h>
+#include <json-glib/json-glib.h>
+#ifdef G_OS_WIN32
+#include <windows.h>
+#endif
+#if HAVE_MPI
+#include <mpi.h>
+#endif
+#if HAVE_GTK
+#include <gio/gio.h>
+#include <gtk/gtk.h>
+#endif
+#include "genetic/genetic.h"
+#include "utils.h"
+#include "experiment.h"
+#include "variable.h"
+#include "input.h"
+#include "optimize.h"
+#if HAVE_GTK
+#include "interface.h"
+#endif
+#include "mpcotool.h"
+
+int
+main (int argn, char **argc)
+{
+#if HAVE_GTK
+  show_pending = process_pending;
+#endif
+  return mpcotool (argn, argc);
+}
