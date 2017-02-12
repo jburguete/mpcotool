86e26df8f9ee4142d65a4762a4d3ce5d260bc6f8
Javier Burguete <jburguete@eead.csic.es>
2017-02-12 22:37:27 +0100

    Improving compilation and external library interface

---
+++ 3.4.1/mpcotool.h
@@ -0,0 +1,43 @@
+/*
+MPCOTool:
+The Multi-Purposes Calibration and Optimization Tool. A software to perform
+calibrations or optimizations of empirical parameters.
+
+AUTHORS: Javier Burguete and Borja Latorre.
+
+Copyright 2012-2016, AUTHORS.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY AUTHORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+/**
+ * \file mpocotool.h
+ * \brief Main function header file.
+ * \authors Javier Burguete and Borja Latorre.
+ * \copyright Copyright 2012-2016, all rights reserved.
+ */
+#ifndef MPCOTOOL__H
+#define MPCOTOOL__H 1
+
+extern int mpcotool (int argn, char **argc);
+
+#endif
