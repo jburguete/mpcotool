6ed7f3ac86bb77c3d4f10aba43780577ec76ef3d
Javier Burguete <jburguete@eead.csic.es>
2023-03-13 13:23:26 +0100

    Improved make links script

--- 4.10.0/make-links.sh
+++ 4.10.0/make-links.sh
@@ -1,9 +1,15 @@
-ln -s ../../jb/jb
-ln -s ../../genetic/genetic
-ln -s jb/bin/libjb-3.so
-ln -s jb/bin/libjbm-3.so
-ln -s jb/bin/libjbjson-3.so
-ln -s jb/bin/libjbxml-3.so
-ln -s jb/bin/libjbbin-3.so
-ln -s jb/bin/libjbwin-3.so
-ln -s genetic/libgenetic.so
+#!/usr/bin/env bash
+if [ `uname -o` != 'Msys' ]; then
+  ln -sf ../../jb/jb
+  ln -sf ../../genetic/genetic
+  so='.so'
+else
+  so='.dll'
+fi
+ln -sf jb/bin/libjb-3$so
+ln -sf jb/bin/libjbm-3$so
+ln -sf jb/bin/libjbjson-3$so
+ln -sf jb/bin/libjbxml-3$so
+ln -sf jb/bin/libjbbin-3$so
+ln -sf jb/bin/libjbwin-3$so
+ln -sf genetic/libgenetic$so
