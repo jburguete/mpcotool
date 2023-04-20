a37e267bd8929c1158c663a025c1e06069ca044b
Javier Burguete <jburguete@eead.csic.es>
2023-04-20 11:55:24 +0200

    Improved building strip to work in MacOS

--- 4.10.1/make-links.sh
+++ 4.10.1/make-links.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-if [ `uname -o` != 'Msys' ]; then
+if [ `uname -s` != 'Msys' ]; then
   ln -sf ../../jb/jb
   ln -sf ../../genetic/genetic
   for i in 2 3 4; do
     ln -sf ../../../genetic/genetic ../tests/test$i
   done
+  if [ `uname -s` != 'Darwin' ]; then
+	  bin=bin/
+  fi
   so='.so'
 else
   so='.dll'
 fi
-ln -sf jb/bin/libjb-3$so
-ln -sf jb/bin/libjbm-3$so
-ln -sf jb/bin/libjbjson-3$so
-ln -sf jb/bin/libjbxml-3$so
-ln -sf jb/bin/libjbbin-3$so
-ln -sf jb/bin/libjbwin-3$so
+ln -sf jb/${bin}libjb-3$so
+ln -sf jb/${bin}libjbm-3$so
+ln -sf jb/${bin}libjbjson-3$so
+ln -sf jb/${bin}libjbxml-3$so
+ln -sf jb/${bin}libjbbin-3$so
+ln -sf jb/${bin}libjbwin-3$so
 ln -sf genetic/libgenetic$so
 for i in 2 3 4; do
   ln -sf genetic/libgenetic$so ../tests/test$i
