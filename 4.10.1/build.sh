098292d151d402bcdf9258f2762ffcde4350b0d4
Javier Burguete <jburguete@eead.csic.es>
2023-05-29 16:02:07 +0200

    Updated build script to improve OpenBSD building

--- 4.10.1/build.sh
+++ 4.10.1/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [[ `uname -s` = "OpenBSD" ]]; then
+	export AUTOCONF_VERSION=2.71
+	export AUTOMAKE_VERSION=1.16
+fi
 if [[ $# != 6 ]]; then
 	echo "The syntax is: ./build.sh A B C D E F"
 	echo "A: 1 on MPI"
